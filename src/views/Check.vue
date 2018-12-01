229c8e45f3036c0c3daf099ade1f76491eb30768
unknown <1169470657@qq.com>
2018-12-01 13:30:52 +0800

    12.1

--- src/views/Check.vue
+++ src/views/Check.vue
@@ -2,9 +2,16 @@
   <div class="CheckContent">
     <div class="courseHtml" v-html="ResHTML"></div>
     <div class="courseCheck">
-      <div v-for="course in courseArr">
-        <input type="checkbox" name="" id="">
-        {{course.name}}
+      <div class="courseBox" v-for="(course, index) in courseArr" key="index">
+        <input type="checkbox" :value="index">
+        <span>{{course.name}}</span>
+        <div>{{course.teacher}}</div>
+        <div class="courseUnitContent">
+          <div class="courseUnit" v-for="item in course.courseUnits">
+            <div>{{item.weeks}}</div>
+            <div>{{item.building}}{{item.room}}</div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -15,20 +22,18 @@ import axios from "axios";
 export default {
   data: function() {
     return {
-      ResHTML: '',
+      ResHTML: "",
       courseArr: []
-    }
+    };
   },
   async mounted() {
-    const getData = await axios.get('http://127.0.0.1:7001/login');
+    const getData = await axios.get("http://127.0.0.1:7001/login");
     console.log(getData.data);
     this.ResHTML = getData.data.html;
     this.courseArr = getData.data.newCourseArr;
   },
-  methods: {
-
-  }
-}
+  methods: {}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -44,5 +49,15 @@ export default {
 }
 .courseCheck {
   flex: 1;
+  padding: 20px;
+}
+.courseBox {
+  margin-bottom: 10px;
+}
+.courseUnitContent {
+  display: flex;
+}
+.courseUnit {
+  margin: 3px 10px;
 }
 </style>
